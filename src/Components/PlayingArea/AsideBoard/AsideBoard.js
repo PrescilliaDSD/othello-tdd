c8bde7e6bc1aa52c67de9849eaaeb279abdb26c3
PrescilliaDSD <dossantosdias.prescillia@gmail.com>
2020-03-25 18:12:48 +0100

    drag part ok

--- src/Components/PlayingArea/AsideBoard/AsideBoard.js
+++ src/Components/PlayingArea/AsideBoard/AsideBoard.js
@@ -1,22 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+
+import AsideChip from "./AsideChip";
 import "./AsideBoard.scss";
 
-const AsideBoard = ({ currentPlayer }) => (
-  <div className="aside-board">
-    <div
-      data-testid="aside-chip"
-      className={`${currentPlayer}-chip chip`}
-    ></div>
-    <button
-      className="aside-board__pass-button"
-      type="button"
-      data-testid="pass-button"
-    >
-      passez
-    </button>
-  </div>
-);
+const AsideBoard = ({ currentPlayer }) => {
+  return (
+    <div className="aside-board">
+      <AsideChip color={currentPlayer} />
+      <button
+        className="aside-board__pass-button"
+        type="button"
+        data-testid="pass-button"
+      >
+        passez
+      </button>
+    </div>
+  );
+};
 
 AsideBoard.propTypes = {
   currentPlayer: PropTypes.string.isRequired
